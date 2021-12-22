fde657efc2effc3b9bb1311c26c28e60cec422e3
shadoedragon <zhao.yichen@outlook.com>
2021-12-22 17:13:40 -0500

    learn cpp

---
+++ three_number_addition.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a,b,c;
+    a=954788;
+    b=843265;
+    c=1001089;
+    float sum;
+    sum=a+b+c;
+    cout<<"a+b+c= "<<sum<<"\n";
+    return 0;
+}
